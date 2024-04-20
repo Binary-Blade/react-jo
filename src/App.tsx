57db4754b790b0c4ec39ea3a1954f782cf7f333f
Binary-Blade <codegrunt@proton.me>
2024-04-20 02:33:07 +0200

    * feat: Implement useEffect session to keep session active link to cookie and refactoring routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import { Switch, Route } from 'wouter';
 import { HomePage } from './pages/HomePage';
-import { TicketOffersPage } from './pages/TicketOffersPage';
-import { AuthPage } from './pages/AuthPage';
+import { TicketOffersPage } from './pages/ticket/TicketOffersPage';
+import { LoginPage } from './pages/auth/LoginPage';
+import { SignUpPage } from './pages/auth/SignUpPage';
+import { ProtectedRoute } from './components/common/auth/ProtectedRoute';
+import { useEffect } from 'react';
+import { useAuthStore } from './stores/useAuthStore';
 
 export default function App() {
+  const initializeSession = useAuthStore(state => state.initializeSession);
+
+  useEffect(() => {
+    initializeSession();  //  Initialize the session when the app starts
+  }, [initializeSession]);
 
   return (
     <>
       <Switch>
         <Route path="/" component={HomePage} />
-        <Route path="/auth" component={AuthPage} />
+        <Route path="/signup" component={SignUpPage} />
+        <Route path="/login" component={LoginPage} />
         <Route path="/offers" component={TicketOffersPage} />
+        <ProtectedRoute path="/cart" component={TicketOffersPage} />
       </Switch>
     </>
   )
