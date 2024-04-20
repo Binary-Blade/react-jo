213ccc731c13448cc060049e6fbb0cd41fc0c39f
Binary-Blade <codegrunt@proton.me>
2024-04-20 02:30:52 +0200

    * feat: Add interceptors axios client

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,10 +1,25 @@
+import { useAuthStore } from '@/stores/useAuthStore';
 import axios from 'axios';
 
 const axiosClient = axios.create({
     baseURL: 'http://localhost', // Votre URL de base API
     headers: {
         'Content-Type': 'application/json'
-    }
+    },
+    withCredentials: true
 });
 
+axiosClient.interceptors.request.use(
+    (config) => {
+        const token = useAuthStore.getState().accessToken;
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+            console.log('Authorization Header Set:', config.headers['Authorization']);
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 export default axiosClient;
