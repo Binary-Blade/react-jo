50f80ab7f6e4f695e068fd9e9861ae025a66037d
Binary-Blade <codegrunt@proton.me>
2024-04-20 02:33:35 +0200

    * refactor: Refactoring routes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,10 @@
 import Events from "@/components/home/Events"
-import Footer from "@/components/ui/Footer"
-import { Header } from "@/components/ui/Header"
 import Hero from "@/components/home/Hero"
 import SectionTickets from "@/components/home/SectionTickets"
 import { CounterDate } from "@/components/home/CounterDate"
 import { PresentSports } from "@/components/home/PresentSports"
+import { Header } from "@/components/common/header/Header"
+import Footer from "@/components/common/footer/Footer"
 
 export const HomePage = () => {
 
