4702817c5dfd03c5c1cfd079545e2437ea47d602
Binary-Blade <codegrunt@proton.me>
2024-04-22 03:56:27 +0200

    * feat: Add EventState Interface

--- src/types/EventTypes.ts
+++ src/types/EventTypes.ts
@@ -1,13 +1,32 @@
 export type EventPropsType = {
+    eventId?: number,
     title: string,
     description: string,
-    imageSrc: string
+    imageSrc?: string,
+    basePrice?: number,
+    quantityAvailable: number
+}
+
+export type Event = {
+    eventId?: number;
+    title: string;
+    description: string;
+    basePrice: number;
+    quantityAvailable: number;
 }
 
 
-export type CreateEventDto = {
-    title: string;
-    description: string;
-    price: number;
-    quantityAvailable: number;
+export type EventResponse = {
+    success: boolean;
+    data: any;
+}
+
+
+export interface EventState {
+    events: Event[];
+    fetchEvents: () => Promise<void>;
+    getTicketPrice: (eventId: number, ticketType: string) => Promise<any>;
+    addEvent: (eventData: Event) => Promise<void>;
+    updateEvent: (eventId: number, eventData: Event) => Promise<void>;
+    deleteEvent: (eventId: number) => Promise<void>;
 }
