50d1f59eb0453f93b9d8202733ef9df36855275f
Binary-Blade <codegrunt@proton.me>
2024-04-22 03:54:47 +0200

    * feat: Create EventsLogic Card and To buy ticket

--- src/components/event/EventCard.tsx
+++ src/components/event/EventCard.tsx
@@ -1,31 +1,32 @@
-import { Link } from "wouter";
-import { Button } from "../ui/button";
 import React from "react";
 import { EventPropsType } from "@/types/EventTypes";
+import { EventDrawerTicket } from "./EventDrawerTicket";
 
-
-export const EventCard: React.FC<EventPropsType> = ({ title, description, imageSrc }) => (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        <div className="group grid gap-4 rounded-lg overflow-hidden bg-gray-100 
-            dark:bg-gray-800 transition-all hover:scale-[1.02] hover:shadow-lg">
+export const EventCard: React.FC<EventPropsType> = ({
+    eventId,
+    title,
+    description,
+    imageSrc,
+    basePrice,
+    quantityAvailable
+}) => {
+    return (
+        <div className="relative group grid gap-4 rounded-lg overflow-hidden bg-gray-100 dark:bg-gray-800 transition-all hover:scale-[1.02] hover:shadow-lg">
             <img
-                alt="Swimming"
+                alt={title}
                 className="object-cover w-full aspect-[3/2] group-hover:opacity-90 transition-opacity"
-                height="200"
                 src={imageSrc}
-                width="300"
             />
             <div className="p-4 flex flex-col gap-2">
-                <h3 className="font-semibold text-lg group-hover:underline">{title}</h3>
-                <p className="text-gray-500 dark:text-gray-400 line-clamp-2">
-                    {description}
-                </p>
-                <Link href="/tickets">
-                    <Button className="w-full" variant="default">
-                        Buy Tickets
-                    </Button>
-                </Link>
+                <EventDrawerTicket
+                    eventId={eventId}
+                    title={title}
+                    description={description}
+                    imageSrc={imageSrc}
+                    quantityAvailable={quantityAvailable}
+                    basePrice={basePrice}
+                />
             </div>
-        </div>
-    </div>
-);
+        </div >
+    );
+};
