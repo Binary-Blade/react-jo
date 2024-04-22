b7bc6723b574758433cb6e665720db6f392bfd89
Binary-Blade <codegrunt@proton.me>
2024-04-22 03:55:15 +0200

    * feat: Add EventsPages

--- src/pages/event/EventsPage.tsx
+++ src/pages/event/EventsPage.tsx
@@ -4,8 +4,19 @@ import imageTicket from '@/assets/images/GamesTicketOptions.svg'
 import { EventCard } from "@/components/event/EventCard";
 import { EventHero } from "@/components/event/EventHero";
 import { EventTitle } from "@/components/event/EventTitle";
+import { useEventStore } from "@/stores/useEventStore";
+import { useEffect } from "react";
 
 export default function EventsPage() {
+
+  const { events, fetchEvents } = useEventStore(state => ({
+    events: state.events,
+    fetchEvents: state.fetchEvents,
+  }));
+
+  useEffect(() => {
+    fetchEvents();
+  }, [fetchEvents]);
 
   return (
     <>
@@ -15,12 +26,21 @@ export default function EventsPage() {
       </div>
       <div className="container grid gap-8 md:py-4 px-4 md:px-6">
         <EventTitle />
-        <EventCard
-          title="Swimming"
-          description="Watch the world's best swimmers compete in the pool."
-          imageSrc={imageTicket}
-        />
-      </div >
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {events.map(event => (
+            <EventCard
+              key={event.eventId}
+              eventId={event.eventId}
+              title={event.title}
+              description={event.description}
+              imageSrc={imageTicket}
+              basePrice={event.basePrice}
+              quantityAvailable={event.quantityAvailable}
+            />
+
+          ))}
+        </div >
+      </div>
       <Footer />
     </>
   )
