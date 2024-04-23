fb05e47e85e29fd3f4099d13654efed6def42b54
Binary-Blade <codegrunt@proton.me>
2024-04-23 23:41:19 +0200

    * refactor: Delete constant cartId and link cartId dynamic to the Zustand

--- src/pages/cart/CartPage.tsx
+++ src/pages/cart/CartPage.tsx
@@ -1,28 +1,14 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import { Header } from "@/components/common/header/Header";
 import Footer from "@/components/common/footer/Footer";
 import useCartStore from "@/stores/useCartStore";
 import { CartTicket } from "@/components/cart/CartTicket";
-import { useAuthStore } from "@/stores/useAuthStore";
-
 
 export const CartPage: FC = () => {
-  const { userId } = useAuthStore(state => ({
-    userId: state.userId,
-  }));
-
-
-  const cartId = 1;
-  const { cartItems, fetchCartItems } = useCartStore(state => ({
-    cartItems: state.cartItems,
-    fetchCartItems: state.fetchCartItems,
-  }));
-
-  useEffect(() => {
-    if (userId && cartId) {
-      fetchCartItems(userId, cartId);
-    }
-  }, [fetchCartItems, userId, cartId]); // Runs only once on component mount if dependencies are empty
+  const { cartItems } = useCartStore(
+    state => ({
+      cartItems: state.cartItems,
+    }));
 
   return (
     <>
