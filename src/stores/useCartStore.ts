73c60c7f3df08793d0c8b975a9b4fdead6bfa1ac
Binary-Blade <codegrunt@proton.me>
2024-04-23 23:42:28 +0200

    * refactor: Add cartId to LocalStorage to create persistant data

--- src/stores/useCartStore.ts
+++ src/stores/useCartStore.ts
@@ -1,25 +1,29 @@
 import { create } from 'zustand';
 import { CartService } from '@/services/CartService';
-import { CartItem, CreateCartItemDto, UpdateCartItemDto } from '@/types/CartTypes';
+import { CartItem, CreateCartItemDto } from '@/types/CartTypes';
+import { getStoredCartId, setStoredCartId } from '@/utils/storeCardId';
 
 interface CartState {
     cartItems: CartItem[];
-    cartId?: number;  // Optional if some carts might not yet have an ID
+    cartId?: number | null;
     fetchCartItems: (userId: number, cartId: number) => Promise<void>;
     addItemToCart: (userId: number, cartItem: CreateCartItemDto) => Promise<void>;
-    updateCartItem: (userId: number, cartId: number, cartItemId: number, updateData: UpdateCartItemDto) => Promise<void>;
+    updateCartItem: (userId: number, cartId: number, cartItemId: number, updateData: number) => Promise<void>;
     removeCartItem: (userId: number, cartId: number, cartItemId: number) => Promise<void>;
 }
 
 export const useCartStore = create<CartState>((set, get) => ({
     cartItems: [],
-    fetchCartItems: async (userId, cartId) => {
-        const data = await CartService.findAllItemsInCart(userId, cartId);
-        set({ cartItems: data });
-    },
+    cartId: getStoredCartId(),
+
     addItemToCart: async (userId, cartItem) => {
         const data = await CartService.addItemToCart(userId, cartItem);
-        set({ cartItems: [...get().cartItems, data] });
+        setStoredCartId(data.cart.cartId);
+        set({ cartItems: [...get().cartItems, data], cartId: data.cart.cartId });
+    },
+    fetchCartItems: async (userId, cartId) => {
+        const data = await CartService.findAllItemsInCart(userId, cartId);
+        set({ cartItems: data, cartId });
     },
     updateCartItem: async (userId, cartId, cartItemId, updateData) => {
         const data = await CartService.updateCartItem(userId, cartId, cartItemId, updateData);
