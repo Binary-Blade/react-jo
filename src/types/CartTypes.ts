7b176ade4527c4b3f05c8d9451b0531c2950599e
Binary-Blade <codegrunt@proton.me>
2024-04-23 06:37:03 +0200

    * refactor: Add types to CartState

--- src/types/CartTypes.ts
+++ src/types/CartTypes.ts
@@ -2,19 +2,28 @@
 // Interface for the functions available in the cart store
 export interface CartState {
     cartItems: CartItem[];
+    cartId: number | null;
     fetchCartItems: (userId: number, cartId: number) => Promise<void>;
     addItemToCart: (userId: number, cartItem: CreateCartItemDto) => Promise<void>;
-    updateCartItem: (userId: number, cartId: number, cartItemId: number, updateData: UpdateCartItemDto) => Promise<void>;
+    updateCartItem: (userId: number, cartId: number, cartItemId: number, quantity: number) => Promise<void>;
     removeCartItem: (userId: number, cartId: number, cartItemId: number) => Promise<void>;
 }
 
 // src/types/CartItemTypes.ts
 export interface CartItem {
     cartItemId: number;
-    eventId: number; // Assuming event ID is needed explicitly
     quantity: number;
+    price: number;
+    ticketType: string;
     createdAt: string;
     updatedAt: string;
+    eventId: number;
+    event: {
+        title: string;
+    };
+    cart: {
+        cartId: number;
+    };
 }
 
 export interface CreateCartItemDto {
@@ -24,4 +33,12 @@ export interface CreateCartItemDto {
 
 export interface UpdateCartItemDto {
     quantity: number;
+}
+
+export interface CartTicketProps {
+    cartItems: CartItem[];
+}
+
+export interface GroupedItems {
+    [key: string]: CartItem[];
 }
