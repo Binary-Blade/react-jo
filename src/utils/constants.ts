239d5cf605b9900a478b508de3c4e2dbccb9afeb
Binary-Blade <codegrunt@proton.me>
2024-04-24 23:24:36 +0200

    * feat: Create constants files adn statuscolor constant export

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const STATUSCOLOR: { [key: string]: string } = {
+    pending: "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-100",
+    approved: "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-100",
+    rejected: "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-100",
+    cancelled: "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-100",
+};
