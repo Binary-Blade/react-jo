3cf6155b32b46d9d41bb4fbeb53e0c649002c82d
Binary-Blade <codegrunt@proton.me>
2024-04-25 03:13:52 +0200

    * refactor: Delete unused interfaces

--- src/types/FormType.ts
+++ src/types/FormType.ts
@@ -1,6 +1,3 @@
-import { LoginFormData } from "@/validation/schemas/loginSchema";
-import { SignupFormData } from "@/validation/schemas/signupSchema";
-
 export interface UserSignupData {
     email: string;
     password: string;
@@ -11,14 +8,5 @@ export interface UserSignupData {
 export interface UserLoginData {
     email: string;
     password: string;
-}
-
-export interface SignUpFormProps {
-    onSubmit: (data: SignupFormData) => void | Promise<void>;
-}
-
-export interface LoginFormProps {
-    onSubmit: (data: LoginFormData) => void | Promise<void>;
-    disabled?: boolean;
 }
 
