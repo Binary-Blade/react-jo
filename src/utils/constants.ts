06b76736e9e8e3ccd3e4e4c081043b5429b06e9f
Binary-Blade <codegrunt@proton.me>
2024-04-26 08:57:33 +0200

    * refactor: Using Uppercase for constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,8 @@
 export const STATUSCOLOR: { [key: string]: string } = {
-    pending: "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-100",
-    approved: "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-100",
-    rejected: "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-100",
-    cancelled: "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-100",
+    PENDING: "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-100",
+    APPROVED: "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-100",
+    REJECTED: "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-100",
+    CANCELLED: "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-100",
 };
+
+export const TAXES_10 = 0.1;
