9b866170f2a1882b63465e18142684937f598ab3
Binary-Blade <codegrunt@proton.me>
2024-05-10 14:54:37 +0200

    * refactor: Modidy the date logic

--- src/components/one-event/MobileTitleEvent.tsx
+++ src/components/one-event/MobileTitleEvent.tsx
@@ -9,7 +9,7 @@ export const MobileTitleEvent: FC<MobileTitleEventProps> = ({ title, eventDate, 
   <div className="flex sm:hidden flex-col items-center gap-1 py-4">
     <h2 className="text-2xl font-semibold">{title}</h2>
     <p className="text-xl sm:text-base text-gray-500 dark:text-gray-400">
-      {eventDate}
+      {eventDate} · Paris, France
     </p>
   </div>
 );
