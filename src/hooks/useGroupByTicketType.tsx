2f752dde9e4911406c3650225d1481e2ac6fb11d
Binary-Blade <codegrunt@proton.me>
2024-05-10 15:18:39 +0200

    * feat: Implement order logic for formule to solo duo and family

--- src/hooks/useGroupByTicketType.tsx
+++ src/hooks/useGroupByTicketType.tsx
@@ -1,10 +1,36 @@
-import { useMemo } from "react";
-import { CartItem, GroupedItems } from "@/config/types/CartTypes";
+import { useMemo } from 'react';
+import { CartItem } from '@/config/types/CartTypes';
+import { PriceFormula } from '@/config/enums/PriceFormula.enum';
 
+interface GroupedItems {
+  [key: string]: CartItem[];
+}
+const priceFormulaOrder: { [key in PriceFormula]: number } = {
+  [PriceFormula.SOLO]: 1,
+  [PriceFormula.DUO]: 2,
+  [PriceFormula.FAMILY]: 3
+};
+
+/**
+ * Group the cart items by price formula and sort them in the correct order.
+ *
+ * @param items The cart items to group.
+ * @returns The grouped items.
+ */
 const useGroupByTicketType = (items: CartItem[]): GroupedItems => {
   return useMemo(() => {
-    return items.reduce((acc: GroupedItems, item) => {
-      (acc[item.ticketType] = acc[item.ticketType] || []).push(item);
+    // Group the items by price formula
+    const grouped = items.reduce((acc: GroupedItems, item) => {
+      (acc[item.priceFormula] = acc[item.priceFormula] || []).push(item);
+      return acc;
+    }, {});
+
+    // Correctly sort the grouped items
+    const sortedKeys = Object.keys(grouped).sort(
+      (a, b) => priceFormulaOrder[a as PriceFormula] - priceFormulaOrder[b as PriceFormula]
+    );
+    return sortedKeys.reduce((acc: GroupedItems, key) => {
+      acc[key] = grouped[key];
       return acc;
     }, {});
   }, [items]);
