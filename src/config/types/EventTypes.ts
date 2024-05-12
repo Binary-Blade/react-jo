9bf41d603295cb826f3f95210258eb361095a874
Binary-Blade <codegrunt@proton.me>
2024-05-12 11:35:03 +0200

    * feat: Implement zod schema and upgrade type for event add dashboard logic

--- src/config/types/EventTypes.ts
+++ src/config/types/EventTypes.ts
@@ -1,46 +1,53 @@
 export type EventPropsType = {
-    eventId?: number | undefined,
-    title: string,
-    description: string,
-    imageSrc?: string,
-    basePrice?: number | undefined,
-    quantityAvailable: number
-}
+  eventId?: number | undefined;
+  title: string;
+  description: string;
+  imageSrc?: string;
+  basePrice?: number | undefined;
+  quantityAvailable: number;
+};
 
 export type EventType = {
-    eventId?: number | undefined;
-    title: string;
-    description: string;
-    basePrice: number | undefined;
-    eventName: string;
-    prices: {
-        eventPriceId: number;
-        priceFormula: string;
-        price: number;
-    };
-    categoryType: string;
-    quantityAvailable: number;
-    quantitySold: number;
-    category: string;
-    revenueGenerated: number;
-    startDate: string;
-    endDate: string;
-}
+  eventId?: number | undefined;
+  title: string;
+  description: string;
+  basePrice: number | undefined;
+  eventName: string;
+  prices: {
+    eventPriceId: number;
+    priceFormula: string;
+    price: number;
+  };
+  categoryType: string;
+  quantityAvailable: number;
+  quantitySold: number;
+  revenueGenerated: number;
+  startDate: string;
+  endDate: string;
+};
 
+export type EventRequest = {
+  title: string;
+  description: string;
+  basePrice: number;
+  categoryType: string;
+  quantityAvailable: number;
+  startDate: string;
+  endDate: string;
+};
 
 export type EventResponse = {
-    success: boolean;
-    data: any;
-}
-
+  success: boolean;
+  data: any;
+};
 
 export interface EventStoreType {
-    events: EventType[];
-    event: EventType | null;
-    fetchEvents: () => Promise<void>;
-    getEvent: (eventId: number) => Promise<any>;
-    getTicketPrice: (eventId: number, ticketType: string) => Promise<any>;
-    addEvent: (eventData: EventType) => Promise<void>;
-    updateEvent: (eventId: number, eventData: EventType) => Promise<void>;
-    deleteEvent: (eventId: number) => Promise<void>;
+  events: EventType[];
+  event: EventType | null;
+  fetchEvents: () => Promise<void>;
+  getEvent: (eventId: number) => Promise<any>;
+  getTicketPrice: (eventId: number, ticketType: string) => Promise<any>;
+  addEvent: (eventData: EventRequest) => Promise<void>;
+  updateEvent: (eventId: number, eventData: EventType) => Promise<void>;
+  deleteEvent: (eventId: number) => Promise<void>;
 }
