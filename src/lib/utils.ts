662bab4eefae5873f6fc4d126d76b50603834069
Binary-Blade <codegrunt@proton.me>
2024-05-14 14:11:47 +0200

    * feat: Add new function to filter properties to return to backend to fit with DTO LOGIC

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,35 +1,44 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function debounce<F extends (...args: any[]) => Promise<void>>(
-    func: F,
-    wait: number,
-    immediate = false
+  func: F,
+  wait: number,
+  immediate = false
 ): (...args: Parameters<F>) => Promise<void> {
-    let timeout: ReturnType<typeof setTimeout> | null;
+  let timeout: ReturnType<typeof setTimeout> | null;
 
-    return function(this: any, ...args: Parameters<F>): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            const later = () => {
-                timeout = null;
-                if (!immediate) {
-                    func.apply(this, args).then(resolve).catch(reject);
-                }
-            };
+  return function (this: any, ...args: Parameters<F>): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const later = () => {
+        timeout = null;
+        if (!immediate) {
+          func.apply(this, args).then(resolve).catch(reject);
+        }
+      };
 
-            const callNow = immediate && !timeout;
-            if (timeout) {
-                clearTimeout(timeout);
-            }
-            timeout = setTimeout(later, wait);
+      const callNow = immediate && !timeout;
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+      timeout = setTimeout(later, wait);
 
-            if (callNow) {
-                func.apply(this, args).then(resolve).catch(reject);
-            }
-        });
-    };
+      if (callNow) {
+        func.apply(this, args).then(resolve).catch(reject);
+      }
+    });
+  };
 }
+
+export const filterProperties = (obj, allowedProps) => {
+  return Object.keys(obj)
+    .filter(key => allowedProps.includes(key))
+    .reduce((acc, key) => {
+      acc[key] = obj[key];
+      return acc;
+    }, {});
+};
