8b089cccf87209d5a80a4c12d536e28342866b5b
Binary-Blade <codegrunt@proton.me>
2024-05-18 20:45:04 +0200

    * feat: Create Empty component ui

--- src/components/empty/ReservationEmpty.tsx
+++ src/components/empty/ReservationEmpty.tsx
@@ -1,19 +1,13 @@
-import { Link } from 'wouter';
 import { CalendarIcon } from '../ui/IconComponents';
 
 export const ReservationEmpty = () => {
   return (
-    <main className="w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-      <section className="flex flex-col items-center justify-center">
-        <CalendarIcon className="w-16 h-16 text-gray-400" />
-        <h2 className="text-xl font-semibold mt-4">No Reservations</h2>
-        <p className="text-gray-500 mt-2">
-          You currently have no reservations. Check back later or make a new reservation.
-        </p>
-        <Link className="mt-4" href="/events">
-          Make a Reservation
-        </Link>
-      </section>
-    </main>
+    <div className="border border-dashed shadow-sm rounded-lg flex flex-col items-center justify-center p-6 text-center bg-white dark:bg-gray-800 col-span-4">
+      <CalendarIcon className="w-16 h-16 text-gray-400" />
+      <h2 className="text-xl font-semibold mt-4">No reservations found</h2>
+      <p className="text-gray-500 dark:text-gray-400 mt-2">
+        Please try adjusting your filters or check back later.
+      </p>
+    </div>
   );
 };
