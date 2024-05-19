0a4a1b2d4852387807d55a94475b247182ab3440
Binary-Blade <codegrunt@proton.me>
2024-05-20 01:17:54 +0200

    * refactor: Restructure useDisplay

--- src/hooks/useDisplayTotalItems.tsx
+++ src/hooks/useDisplayTotalItems.tsx
@@ -1,43 +1,15 @@
-import { CreateCartItemLocalDto } from '@/config/dtos/LocalCartItem.dto';
-import { useAuthStore } from '@/stores/useAuthStore';
-import useCartStore from '@/stores/useCartStore';
-import useLocalCartStore from '@/stores/useLocalCartStore';
 import { useMemo } from 'react';
+import useLocalCartStore from '@/stores/useLocalCartStore';
 
-const useTakeQuantityFromLocalOrServer = () => {
+const useTakeQuantityFromLocal = () => {
   const { cartItemsLocal } = useLocalCartStore();
-  const { cartItems } = useCartStore();
-  const { isAuthenticated } = useAuthStore();
 
   return useMemo(() => {
-    if (isAuthenticated) {
-      // Combine local and online cart items
-      const combinedItems: CreateCartItemLocalDto[] = [...cartItemsLocal];
-
-      cartItems.forEach((item: any) => {
-        const existingItem = combinedItems.find(
-          localItem =>
-            localItem.eventId === item.eventId && localItem.priceFormula === item.priceFormula
-        );
-
-        if (existingItem) {
-          existingItem.quantity += item.quantity;
-        } else {
-          combinedItems.push(item);
-        }
-      });
-
-      return combinedItems;
-    }
-
     return cartItemsLocal;
-  }, [cartItems, cartItemsLocal, isAuthenticated]);
+  }, [cartItemsLocal]);
 };
 
 export const useDisplayTotalItems = () => {
-  const combinedItems = useTakeQuantityFromLocalOrServer();
-  return useMemo(
-    () => combinedItems.reduce((acc, item) => acc + item.quantity, 0),
-    [combinedItems]
-  );
+  const localItems = useTakeQuantityFromLocal();
+  return useMemo(() => localItems.reduce((acc, item) => acc + item.quantity, 0), [localItems]);
 };
