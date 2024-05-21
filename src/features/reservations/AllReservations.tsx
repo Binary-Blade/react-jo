6e704b59c11b410ec3dded810144fbf3b16f604d
Binary-Blade <codegrunt@proton.me>
2024-05-21 22:36:49 +0200

    * refactor: Transform to fetch the Transaction link to reservation instead to fetch reservation for user client

--- src/features/reservations/AllReservations.tsx
+++ src/features/reservations/AllReservations.tsx
@@ -1,43 +1,45 @@
 import { useEffect } from 'react';
-import useReservationStore from '@/stores/useReservationStore';
 import { useAuthStore } from '@/stores/useAuthStore';
 import { useFilter, usePagination } from '@/hooks';
 import { PaginationComponent } from '@/components/pagination/PaginationComponent';
-import { FILTERS_EVENT } from '@/config/filters/filtersEvents';
-import { SORTING_RESERVATION_PUBLIC } from '@/config/sorting/sortingReservation';
 import { ReservationEmpty } from '@/components/empty/ReservationEmpty';
 import { Separator } from '@/components/ui/separator';
 import { CardReservations } from './CardReservations';
 import { FilterDropdown } from '../filter-sorting/FilterDropdown';
 import { SortOrderDropdown } from '../filter-sorting/SortOrderDropdown';
 import { SortByDropdown } from '../filter-sorting/SortByDropdown';
+import { useTransactionStore } from '@/stores/useTransactionStore';
+import { STATUSCOLOR } from '@/config/constants';
+import { Card } from '@/components/ui/card';
+import { FaCalendarAlt, FaEuroSign, FaReceipt } from 'react-icons/fa';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
+import { ChevronRightIcon } from '@/components/ui/IconComponents';
+import { FILTER_OPTIONS_TRANSACTIONS } from '@/config/filters/filtersTransactions';
+import { SORTING_OPTIONS_TRANSACTIONS } from '@/config/sorting/sortingTransactions';
 
 export const AllReservations = () => {
-  const { reservations, fetchReservations, total } = useReservationStore();
+  const { transactions, fetchTransactions, total } = useTransactionStore();
   const { userId } = useAuthStore();
   const { currentPage, setPage, limit, totalPages, offset } = usePagination({
     initialPage: 1,
-    initialLimit: 12,
+    initialLimit: 5,
     totalCount: total
   });
 
   const { sortBy, setSortBy, sortOrder, setSortOrder, filterBy, filterValue, setFilterValue } =
-    useFilter('reservationDetails.price', 'DESC', 'reservationDetails.event.categoryType', 'ALL');
+    useFilter('createdAt', 'DESC', 'statusPayment', 'ALL');
+
+  useEffect(() => {
+    setPage(1); // Reset to the first page whenever filters or sorting change
+  }, [setPage, sortBy, sortOrder, filterBy, filterValue]);
 
   useEffect(() => {
     if (userId) {
-      fetchReservations(userId, {
-        limit,
-        offset,
-        sortBy,
-        sortOrder,
-        filterBy,
-        filterValue
-      });
+      fetchTransactions(userId, { limit, offset, sortBy, sortOrder, filterBy, filterValue });
     }
   }, [
     currentPage,
-    fetchReservations,
+    fetchTransactions,
     userId,
     limit,
     offset,
@@ -49,34 +51,78 @@ export const AllReservations = () => {
 
   useEffect(() => {
     setPage(1); // Reset to the first page whenever filters or sorting change
-  }, [setPage, sortBy, sortOrder, filterBy, filterValue]);
+  }, [setPage]);
 
   return (
     <>
       <section className="p-10">
-        <div className="flex items-center justify-between ">
-          <div className="flex items-center space-x-4 ">
-            <SortByDropdown
-              sortBy={sortBy}
-              onSortChange={setSortBy}
-              options={SORTING_RESERVATION_PUBLIC}
-            />
-            <SortOrderDropdown sortOrder={sortOrder} setSortOrder={setSortOrder} />
-            <FilterDropdown
-              groups={FILTERS_EVENT}
-              filterValue={filterValue}
-              onChange={setFilterValue}
-            />
-          </div>
+        <div className="flex items-center space-x-4">
+          <SortByDropdown
+            sortBy={sortBy}
+            onSortChange={setSortBy}
+            options={SORTING_OPTIONS_TRANSACTIONS}
+          />
+          <SortOrderDropdown sortOrder={sortOrder} setSortOrder={setSortOrder} />
+          <FilterDropdown
+            groups={FILTER_OPTIONS_TRANSACTIONS}
+            filterValue={filterValue}
+            onChange={setFilterValue}
+          />
         </div>
       </section>
-      <div className="px-10 pb-10 grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {reservations.map(reservation => (
-          <CardReservations key={reservation.reservationId} reservation={reservation} />
-        ))}
+      <div className="px-4 space-y-8">
+        {transactions.length > 0 ? (
+          transactions.map(transaction => (
+            <div key={transaction.transactionId} className="mb-6">
+              <div className="mb-4 flex flex-row justify-between items-center">
+                <h3 className="text-2xl font-bold flex items-center">
+                  <FaReceipt className="mr-2 text-rose-600" /> Transaction #
+                  {transaction.transactionId}
+                </h3>
+                <div
+                  className={`px-3 py-1 rounded-md border text-md font-semibold ${STATUSCOLOR[transaction.statusPayment]}`}
+                >
+                  {transaction.statusPayment}
+                </div>
+              </div>
+              <div className="px-3">
+                <div className="text-md font-semibold flex items-center mb-2">
+                  <FaCalendarAlt className="mr-2 text-gray-600" />
+                  Date d'achat:
+                  <span className="text-sm text-gray-500 ml-1">{transaction.createdAt}</span>
+                </div>
+                <div className="text-md font-semibold flex items-center mb-4">
+                  <FaEuroSign className="mr-2 text-gray-600" />
+                  Montant total:
+                  <span className="text-sm text-gray-500 ml-1">{transaction.totalAmount}€</span>
+                </div>
+              </div>
+              <Collapsible>
+                <CollapsibleTrigger className="font-semibold flex items-center gap-1 transition-transform duration-300 ease-in-out [&[data-state=open]>svg]:rotate-90">
+                  Voir plus
+                  <ChevronRightIcon className="w-4 h-4 translate-y-px transition-transform duration-300 ease-in-out" />
+                </CollapsibleTrigger>
+                <CollapsibleContent className="transition-opacity duration-300 ease-in-out">
+                  <div className=" rounded-lg overflow-hidden py-8">
+                    <div className="px-2 pb-4 grid gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-5">
+                      {transaction.reservation.map(reservation => (
+                        <CardReservations
+                          key={reservation.reservationId}
+                          reservation={reservation}
+                          statusPayment={transaction.statusPayment}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </CollapsibleContent>
+              </Collapsible>
+              <Separator className="my-6" />
+            </div>
+          ))
+        ) : (
+          <ReservationEmpty />
+        )}
       </div>
-      <div>{reservations.length <= 0 && <ReservationEmpty />}</div>
-      <Separator />
       <PaginationComponent
         currentPage={currentPage}
         totalPages={totalPages}
--- src/features/reservations/CardReservations.tsx
+++ src/features/reservations/CardReservations.tsx
@@ -1,57 +1,62 @@
 import { DialogTrigger, Dialog, DialogContent } from '@/components/ui/dialog';
-import { Card, CardTitle, CardHeader, CardContent } from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import { CalendarIcon, TableIcon, TicketIcon } from '@/components/ui/IconComponents';
-import { STATUSCOLOR } from '@/config/constants';
+import { CalendarIcon, MapIcon, TicketIcon } from '@/components/ui/IconComponents';
 import { FC } from 'react';
 import { TicketModal } from '@/features/ticket/TicketModal';
+import { Badge } from '@/components/ui/badge';
+import { EuroIcon } from 'lucide-react';
+import { TbCategoryMinus } from 'react-icons/tb';
 
 type CardReservationsProps = {
   reservation: any;
+  statusPayment: string;
 };
 
-export const CardReservations: FC<CardReservationsProps> = ({ reservation }) => {
+export const CardReservations: FC<CardReservationsProps> = ({ reservation, statusPayment }) => {
   return (
     <>
       <Card
         id={reservation.reservationId}
         className="group relative overflow-hidden rounded-lg border border-gray-200 bg-white transition-all 
-        duration-300 hover:-translate-y-1 hover:shadow-lg dark:border-gray-800 dark:bg-gray-950"
+        duration-300 hover:-translate-y-1 hover:shadow-lg hover:border-rose-500 "
       >
-        <CardHeader>
-          <div className="flex items-center justify-between gap-10">
-            <CardTitle className="text-xl">{reservation.reservationDetails?.title}</CardTitle>
-            <div
-              className={`px-2 py-1 rounded-md border text-sm font-medium ${STATUSCOLOR[reservation.transaction.statusPayment]}`}
-            >
-              {reservation.transaction.statusPayment}
+        <div className="flex flex-row justify-between p-2">
+          <div className="flex flex-row ">
+            <div className="flex items-center gap-1 text-md font-semibold">
+              <TicketIcon className="w-5 h-5 text-rose-500" />
+              {reservation.reservationDetails?.title}
             </div>
           </div>
-          <div className="flex items-center gap-2">
-            <div className="text-sm text-gray-500 dark:text-gray-400">
-              Ref: {reservation.user?.userId}-{reservation.reservationId}-
-              {reservation.transaction.paymentId}
-            </div>
-          </div>
-        </CardHeader>
+          <Badge className="text-xs">{reservation.reservationDetails?.priceFormula}</Badge>
+        </div>
         <CardContent>
+          <div className="flex items-center gap-2 text-sm text-gray-500 dark:text-gray-400">
+            <TbCategoryMinus className="h-4 w-4" />
+            <p>{reservation.reservationDetails.event.categoryType}</p>
+          </div>
           <div className="flex items-center gap-2 text-sm text-gray-500 dark:text-gray-400">
             <CalendarIcon className="h-4 w-4" />
             <span>{reservation.reservationDetails.event.startDate}</span>
           </div>
-          <div className="mt-2 flex items-center gap-2 text-sm text-gray-500 dark:text-gray-400">
-            <TableIcon className="w-4 h-4" />
-            <span>{reservation.reservationDetails?.priceFormula} Formula</span>
+          <div className="flex items-center gap-2 text-sm text-gray-500 dark:text-gray-400">
+            <MapIcon className="h-4 w-4" />
+            <span>Paris</span>
           </div>
-          <div className="mt-4 flex items-center justify-between">
-            <div className="text-lg font-semibold">
-              €{reservation.reservationDetails.price.toFixed(2)}
-            </div>
+          <div className="flex items-center gap-2 text-sm text-gray-500 dark:text-gray-400">
+            <EuroIcon className="h-4 w-4" />
+            <span>{reservation.reservationDetails.price.toFixed(2)}</span>
+          </div>
+          <div className="mt-4 flex items-center justify-center">
             <div className="flex items-center gap-2">
-              {reservation.transaction.statusPayment === 'APPROVED' && (
+              {statusPayment === 'APPROVED' && (
                 <Dialog>
                   <DialogTrigger asChild>
-                    <Button size="sm" variant="outline" className="gap-2">
+                    <Button
+                      size="sm"
+                      variant="outline"
+                      className="gap-2 hover:bg-rose-500 hover:text-white"
+                    >
                       <TicketIcon className="w-4 h-4" />
                       View Ticket
                     </Button>
