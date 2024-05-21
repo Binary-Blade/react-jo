d4b6d5ca877dff206adbc19e52deb4cea54ff2c1
Binary-Blade <codegrunt@proton.me>
2024-05-21 02:19:58 +0200

    * feat: Add soonner and toaster for notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './assets/styles/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './assets/styles/index.css';
+import { Toaster } from '@/components/ui/toaster';
+import { SonnerToast } from './components/ui/sonner.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Toaster />
+    <SonnerToast />
+  </React.StrictMode>
+);
