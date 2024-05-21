08b8b4d4873720be8f98cd8f2220a0a1d35029b1
Binary-Blade <codegrunt@proton.me>
2024-05-22 00:41:50 +0200

    * refactor: Modify to show data date in french language

--- src/hooks/useFormattedDates.tsx
+++ src/hooks/useFormattedDates.tsx
@@ -1,4 +1,5 @@
 import { format, parseISO } from 'date-fns';
+import { fr } from 'date-fns/locale';
 
 export const useFormattedDates = (
   startDate: string | undefined,
@@ -6,24 +7,24 @@ export const useFormattedDates = (
 ): string | undefined => {
   // Ensure dates are defined and are valid ISO strings
   if (!startDate || !endDate) {
-    return 'Invalid date range';
+    return 'Intervalle de dates invalide';
   }
 
   try {
-    const formattedStartDate = format(parseISO(startDate), 'MMMM d');
-    const formattedEndDate = format(parseISO(endDate), 'MMMM d, yyyy');
+    const formattedStartDate = format(parseISO(startDate), 'd MMMM', { locale: fr });
+    const formattedEndDate = format(parseISO(endDate), 'd MMMM yyyy', { locale: fr });
 
-    const startYear = format(parseISO(startDate), 'yyyy');
-    const endYear = format(parseISO(endDate), 'yyyy');
+    const startYear = format(parseISO(startDate), 'yyyy', { locale: fr });
+    const endYear = format(parseISO(endDate), 'yyyy', { locale: fr });
 
     if (startYear === endYear) {
       return `${formattedStartDate} - ${formattedEndDate}`;
     } else {
-      const formattedStartDateWithYear = format(parseISO(startDate), 'MMMM d, yyyy');
+      const formattedStartDateWithYear = format(parseISO(startDate), 'd MMMM yyyy', { locale: fr });
       return `${formattedStartDateWithYear} - ${formattedEndDate}`;
     }
   } catch (error) {
-    console.error('Error formatting dates:', error);
-    return 'Error formatting dates';
+    console.error('Erreur de formatage des dates:', error);
+    return 'Erreur de formatage des dates';
   }
 };
