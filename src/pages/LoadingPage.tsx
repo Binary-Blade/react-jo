99582caa876165d4225fb9e7be54df830ec3872e
Binary-Blade <codegrunt@proton.me>
2024-05-22 21:53:49 +0200

    * style: Modify loading page color

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -5,25 +5,16 @@ export default function LoadingPage() {
     <div className="flex h-screen w-full items-center justify-center ">
       <div className="space-y-6 text-center">
         <div className="flex items-center justify-center">
-          <MedalIcon className="h-20 w-20 text-black" />
+          <MedalIcon className="h-20 w-20 text-rose-600" />
         </div>
         <h1 className="text-4xl font-bold tracking-tight text-black sm:text-5xl md:text-6xl">
           Paris 2024
         </h1>
         <div className="flex items-center justify-center" />
         <div className="w-full gap-x-2 flex justify-center items-center">
-          <div
-            className="w-5 bg-blue-600 animate-pulse h-5 rounded-full animate-bounce"
-            role="loading"
-          ></div>
-          <div
-            className="w-5 bg-black-600 animate-pulse h-5 bg-white rounded-full animate-bounce"
-            role="loading"
-          ></div>
-          <div
-            className="w-5 h-5 animate-pulse bg-red-600 rounded-full animate-bounce"
-            role="loading "
-          ></div>
+          <div className="w-4 h-4 rounded-full bg-blue-600 animate-bounce [animation-delay:.7s]" />
+          <div className="w-4 h-4 rounded-full bg-gray-900 animate-bounce [animation-delay:.3s]" />
+          <div className="w-4 h-4 rounded-full bg-rose-600 animate-bounce [animation-delay:.7s]" />
         </div>
       </div>
     </div>
