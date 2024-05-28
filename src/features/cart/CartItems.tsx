2164cf08facca19e0a336cfea1bad60e5be295ee
Binary-Blade <codegrunt@proton.me>
2024-05-28 13:49:15 +0200

    * style: Modify placement button delete

--- src/features/cart/CartItems.tsx
+++ src/features/cart/CartItems.tsx
@@ -49,6 +49,14 @@ export const GroupedCartItems = ({
                   {/* Quantity modification buttons */}
                   <div className="flex items-center gap-2">
                     <Button
+                      size="icon"
+                      variant="outline"
+                      onClick={() => handleDelete(item)}
+                      className="hover:bg-red-500 hover:text-white transition-colors duration-200"
+                    >
+                      <TrashIcon className="w-5 h-5" />
+                    </Button>
+                    <Button
                       onClick={() => handleUpdateQuantity(item, Math.max(1, item.quantity - 1))}
                       disabled={item.quantity === 1}
                       className="px-4 py-1 text-sm border border-gray-300 text-black bg-white hover:bg-rose-500 hover:text-white rounded transition-colors duration-200"
@@ -70,14 +78,6 @@ export const GroupedCartItems = ({
                 <div className="text-gray-900 dark:text-gray-100 font-semibold">
                   {`€${item.price * item.quantity}`}
                 </div>
-                <Button
-                  size="icon"
-                  variant="outline"
-                  onClick={() => handleDelete(item)}
-                  className="hover:bg-red-500 hover:text-white transition-colors duration-200"
-                >
-                  <TrashIcon className="w-5 h-5" />
-                </Button>
               </div>
             ))}
           </div>
